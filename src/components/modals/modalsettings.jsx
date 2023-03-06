30e2b20e17cb7bfd789756440985beb4a5e5e3f7
backstabslash <mr.toppanamor1@gmail.com>
2023-03-06 21:06:50 +0200

    netlify deploy xdxd

--- src/components/modals/modalsettings.jsx
+++ src/components/modals/modalsettings.jsx
@@ -1,38 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './modal.scss';
-import './modalsettings.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./modal.scss";
+import "./modalsettings.css";
 
 function ModalSettings({ openSettings, setOpenSettings, onChangeMode }) {
-    const handleChange = (e) => {
-        if (parseInt(localStorage.getItem("curRow")) !== 0) e.preventDefault();
-        else onChangeMode(e.target.checked);
-    }
-    let hardmode = localStorage.getItem("hardmode") || false;
-    if (hardmode === 'false') hardmode = false;
-    else hardmode = true;
-    return (
-        <div className={`overlay animated ${openSettings ? 'show' : ''}`} onClick={() => setOpenSettings(false)}>
-            <div className="modal" onClick={e => e.stopPropagation()} >
-                <div className='headWrapper'>
-                    <div className='headerTDiv'>settings</div>
-                    <div className='headerIDiv'>
-                        <i className="fa fa-times" aria-hidden="true" onClick={() => setOpenSettings(false)}></i>
-                    </div>
-                </div>
-                <span className='textBlock'> <span className='text'>hard mode
-                    <span className='minitext'><br></br>(any revealed hints must be used in subsequent guesses)</span></span>
-                    <label className="switch">
-                        <input type="checkbox" onClick={handleChange} defaultChecked={hardmode} />
-                        <span className="slider"></span>
-                    </label>
-                </span>
-            </div>
-        </div>)
+  const handleChange = (e) => {
+    if (parseInt(localStorage.getItem("curRow")) !== 0) {
+      e.preventDefault();
+      alert("do not fool me");
+    } else onChangeMode(e.target.checked);
+  };
+  let hardmode = localStorage.getItem("hardmode") || false;
+  if (hardmode === "false") hardmode = false;
+  else hardmode = true;
+  return (
+    <div
+      className={`overlay animated ${openSettings ? "show" : ""}`}
+      onClick={() => setOpenSettings(false)}
+    >
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <div className="headWrapper">
+          <div className="headerTDiv">settings</div>
+          <div className="headerIDiv">
+            <i
+              className="fa fa-times"
+              aria-hidden="true"
+              onClick={() => setOpenSettings(false)}
+            ></i>
+          </div>
+        </div>
+        <span className="textBlock">
+          {" "}
+          <span className="text">
+            hard mode
+            <span className="minitext">
+              <br></br>(any revealed hints must be used in subsequent guesses)
+            </span>
+          </span>
+          <label className="switch">
+            <input
+              type="checkbox"
+              onClick={handleChange}
+              defaultChecked={hardmode}
+            />
+            <span className="slider"></span>
+          </label>
+        </span>
+      </div>
+    </div>
+  );
 }
 
 ModalSettings.propTypes = {
-    onChangeMode: PropTypes.func
-}
+  onChangeMode: PropTypes.func,
+};
 
 export default ModalSettings;
